b0cc12889582b6fc31748ea36363acb01baa1369
= <slotuser@gmail.com>
2019-12-18 14:11:58 +0100

    Todo List : fonctionnel. OOP tache, suppression tache, fonction format date...

--- javascript/todolist/main.js
+++ javascript/todolist/main.js
@@ -1,43 +1,108 @@
+/*Retourne la date du jour, formatée en string 
+en mode année-mois-jour si le format en arg est 'us' ou
+en mode jour-mois-année si le format en arg est 'fr'*/
+function currentDateFormated(format = 'us') {
+	let date = new Date();
+	dateYear = date.getFullYear();
+	/*On rajoute 1 car getMonth() renvoie entre 0 et 11 et non 1 et 12*/
+	dateMonth = date.getMonth()+1;
+	dateDay = date.getDate();
+	let formatedDate;
+	if (format === 'us') {
+		formatedDate = dateYear + '-' + dateMonth + '-' + dateDay;
+	} else if (format === 'fr') {
+		formatedDate = dateDay + '-' + dateMonth + '-' + dateYear;
+	}
+	return formatedDate;
+}
+
+function Task(pDesc, pDate, parent) {
+	const task = document.createElement('li');
+	task.desc = pDesc;
+	task.date = pDate;
+	task.textContent = task.desc + '. Avant le ' + task.date;
+
+	task.append = function() {
+		parent.appendChild(task);
+	}
+
+	task.createDelbox = function() {
+		const delBox = document.createElement('button');
+		task.delBox = delBox;
+		task.delBox.textContent = "Supprimer";
+		task.delBox.style.marginLeft = "10px";
+
+		task.delBox.addEventListener('click', function(e) {
+			task.remove();
+		});
+
+		task.appendChild(task.delBox);
+		return task.delBox;
+	}
+
+	return task;
+}
+
+function reset(pContainer, pFields) {
+	while (pContainer.firstChild) {
+		pContainer.removeChild(pContainer.firstChild);
+	}
+	/*On vide les champs (inputs)*/
+	pFields.forEach(e => e.value = null);
+}
+
 document.body.style.fontFamily = "arial";
 
-let form = document.createElement('form');
-document.body.appendChild(form);
+let main = document.getElementsByTagName('main')[0];
 
+/*Input description de la tâche*/
 let taskDesc = document.createElement('input');
 taskDesc.placeholder = "Faire...";
-document.body.appendChild(taskDesc);
+taskDesc.type = "text";
+main.appendChild(taskDesc);
+/******************************/
 
+/*Input date de la tâche (avant le...)*/
 let taskDate = document.createElement('input');
-taskDate.placeholder = "Avant le...";
-document.body.appendChild(taskDate);
+/*input de type "date" non supporté par Safari*/
+taskDate.type = "date";
+taskDate.min = currentDateFormated('us');
+taskDate.style.verticalAlign = "top";
+main.appendChild(taskDate);
+/*************************************/
 
+/*Bouton creer une tâche*/
 let submitButton = document.createElement('button');
 submitButton.textContent = "Creer tâche";
-document.body.appendChild(submitButton);
+main.appendChild(submitButton);
+/************************/
 
+/*Bouton vider la liste des tâches et vider les inputs*/
 let purgeButton = document.createElement('button');
 purgeButton.textContent = "Vider la liste";
-document.body.appendChild(purgeButton);
+main.appendChild(purgeButton);
+/******************************************************/
 
+/*Container (ul) qui contiendra les tâches (li)*/
 let taskContainer = document.createElement('ul');
-document.body.appendChild(taskContainer);
+main.appendChild(taskContainer);
+/***********************************************/
 
-let task = {};
-
-let dateRegex = /\d{2}\/\d{2}\/\d{4}/;
+let task;
 
 submitButton.addEventListener('click', function() {
-	if (taskDesc.value !== "" && dateRegex.test(taskDate.value)) {
-		task = document.createElement('li');
-		task.textContent = taskDesc.value + ". Avant le " + taskDate.value;
-		taskContainer.appendChild(task);
+	/*Initialisation d'un objet tâche*/
+	task = Task(taskDesc.value, taskDate.value, taskContainer);
+
+	if (task.desc !== "" && task.date !== "") {
+		/*Accrochage de l'objet tâche a son parent (ici taskContainer) = affichage*/
+		task.append();
+		/*Attache d'un bouton de suppression à la tâche*/
+		task.createDelbox();
 	}
+
 });
 
 purgeButton.addEventListener('click', function() {
-	while (taskContainer.firstChild) {
-		taskContainer.removeChild(taskContainer.firstChild);
-	}
-	taskDesc.value = null;
-	taskDate.value = null;
+	reset(taskContainer, [taskDesc, taskDate]);
 });
