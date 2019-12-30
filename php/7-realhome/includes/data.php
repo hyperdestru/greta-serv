1a80b718eef9cdbf8b2df1dcb8f404fbf0f30d09
= <slotuser@gmail.com>
2019-12-30 19:35:04 +0100

    Init activity. Done php data and most of html+php skeleton.

---
+++ php/7-realhome/includes/data.php
@@ -0,0 +1,137 @@
+<?php
+
+$sellingPoints = [
+	[
+		'icon' => 'earth.png',
+		'title' => 'Whole world',
+		'textContent' => 'Proin umcorper urna et felisstibulum iaculis lacinia 
+			est. Proin dictum elem entum velit fusce euismod. 
+			Aenean commodo ligula eget dolor.'
+	],
+
+	[
+		'icon' => 'suitcase.png',
+		'title' => 'Confidentially',
+		'textContent' => 'Proin umcorper urna et felisstibulum iaculis lacinia 
+			est. Proin dictum elem entum velit fusce euismod. 
+			Aenean commodo ligula eget dolor.'
+	],
+
+	[
+		'icon' => 'pictures.png',
+		'title' => 'Good previews',
+		'textContent' => 'Proin umcorper urna et felisstibulum iaculis lacinia 
+			est. Proin dictum elem entum velit fusce euismod. 
+			Aenean commodo ligula eget dolor.'
+	],
+
+	[
+		'icon' => 'drawer.png',
+		'title' => 'Confidentially',
+		'textContent' => 'Proin umcorper urna et felisstibulum iaculis lacinia 
+			est. Proin dictum elem entum velit fusce euismod. 
+			Aenean commodo ligula eget dolor.'
+	],
+];
+
+$houses = [
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '1450 Cloudcroft Drop',
+		'state' => 'Illinois',
+		'city' => 'Chicago',
+		'price' => 250000,
+		'description' => ['3400 Sq Ft', '2 Bedrooms', '1 Bathroom'],
+		'highlight' => true,
+		'featured' => true
+	],
+
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '120 Anastasia Avenue',
+		'state' => 'Florida',
+		'city' => 'Coral gables',
+		'price' => 625000,
+		'description' => ['4600 Sq Ft', '5 Bedrooms', '2 Bathrooms'],
+		'highlight' => true,
+		'featured' => false
+	],
+
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '1450 Cloudcroft Drop',
+		'state' => 'Illinois',
+		'city' => 'Chicago',
+		'price' => 250000,
+		'description' => ['3400 Sq Ft', '2 Bedrooms', '1 Bathroom'],
+		'highlight' => true,
+		'featured' => true
+	],
+
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '1450 Cloudcroft Drop',
+		'state' => 'Illinois',
+		'city' => 'Chicago',
+		'price' => 250000,
+		'description' => ['3400 Sq Ft', '2 Bedrooms', '1 Bathroom'],
+		'highlight' => false,
+		'featured' => true
+	],
+
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '1450 Cloudcroft Drop',
+		'state' => 'Illinois',
+		'city' => 'Chicago',
+		'price' => 250000,
+		'description' => ['3400 Sq Ft', '2 Bedrooms', '1 Bathroom'],
+		'highlight' => false,
+		'featured' => true
+	],
+
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '1450 Cloudcroft Drop',
+		'state' => 'Illinois',
+		'city' => 'Chicago',
+		'price' => 250000,
+		'description' => ['3400 Sq Ft', '2 Bedrooms', '1 Bathroom'],
+		'highlight' => false,
+		'featured' => true
+	],
+
+	[	
+		'img' => 'house1.jpg',
+		'slideImg' => 'slide1.jpg',
+		'address' => '1450 Cloudcroft Drop',
+		'state' => 'Illinois',
+		'city' => 'Chicago',
+		'price' => 250000,
+		'description' => ['3400 Sq Ft', '2 Bedrooms', '1 Bathroom'],
+		'highlight' => true,
+		'featured' => true
+	]
+];
+
+$highlightHouses = [];
+
+foreach ($houses as $key => $house) {
+	if ($house['highlight'] === true) {
+		$highlightHouses[$key] = $house;
+	}
+}
+
+$featuredHouses = [];
+
+foreach ($houses as $key => $house) {
+	if ($house['featured'] === true) {
+		$featuredHouses[$key] = $house;
+	}
+}
---
+++ php/7-realhome/index.php
@@ -0,0 +1,111 @@
+<?php
+	require_once('includes/data.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<title>Real Home Experience</title>
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta http-quiv="X-UA-Compatible" content="ie-edge">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="style/reset.css">
+	<link rel="stylesheet" href="style/main-style.css">
+</head>
+
+<body>
+	<div id="global-wrapper">
+		<header>
+			<div id="header-wrapper">
+				<div class="logo">
+					<a href=""><img src="assets/images/logo.png"></a>
+				</div>
+
+				<nav>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">About us</a></li>
+						<li><a href="">Property</a></li>
+						<li><a href="">Our blog</a></li>
+						<li><a href="">Contacts</a></li>
+					</ul>
+				</nav>
+
+				<ul class="social-icons">
+					<li>
+						<a href=""><img src="assets/icons/facebook.png"></a>
+					</li>
+
+					<li>
+						<a href=""><img src="assets/icons/pinterest.png"></a>
+					</li>
+
+					<li>
+						<a href=""><img src="assets/icons/instagram.png"></a>
+					</li>
+				</ul>
+			</div>
+		</header>
+
+		<main>
+			<section class="slide">
+				<?php foreach ($highlightHouses as $key => $hlHouse) { ?>
+					<div>
+						<p>
+							<?php 
+								echo $hlHouse['address'] .", ". $hlHouse['city'];
+							?>
+							<span>
+								<?php echo "$" . number_format($hlHouse['price'])?>
+							</span>
+						</p>
+						<button>More info</button>
+					</div>
+					<div>
+						<input type="radio" name="slideHouse">
+					</div>
+				<?php } ?>
+			</section>
+
+			<section class="selling-points">
+				<?php foreach ($sellingPoints as $point) { ?>
+					<div>
+						<img src="<?php echo "assets/icons/" . $point['icon']; ?>">
+						<h3><?php echo $point['title']; ?></h3>
+						<p><?php echo $point['textContent']; ?></p>
+					<div>
+				<?php } ?>		
+			</section>
+
+			<section class="featured-properties">
+				<h2>Featured properties</h2>
+				<p>Quisque diam lorem</p>
+
+				<?php foreach ($featuredHouses as $key => $ftHouse) { ?>
+					<article>
+						<img src="<?php echo "assets/images/" . $ftHouse['img']; ?>">
+						<p>
+							<?php echo $ftHouse['address']?>
+							<span><?php echo $ftHouse['state'] ."/". $ftHouse['city']; ?></span>
+							<span><?php echo "$" . number_format($ftHouse['price']); ?></span>
+						</p>
+						<ul>
+							<?php foreach ($ftHouse['description'] as $value) { ?>
+								<li><?php echo $value; ?></li>
+							<?php } ?>
+						</ul>
+					</article>
+				<?php } ?>
+			</section>
+		</main>
+
+		<footer>
+		</footer>
+	</div>
+</body>
+
+</html>
