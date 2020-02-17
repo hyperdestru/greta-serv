4a497c6ceeb10aa754c1a91792e683b3146bd48e
= <slotuser@gmail.com>
2020-02-17 16:37:21 +0100

    Renamed 8-movies.

--- php/8-movies/includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Debug mode
-define('DEBUG', true);
-
-// Database informations
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'gr_movies');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
